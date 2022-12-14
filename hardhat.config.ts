39f15f7e5bc0af52a2bf306fc16b1e89239d5478
kartojal <canillas.mail@gmail.com>
2022-12-14 13:50:59 +0100

    fix: reset localhost config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -63,8 +63,7 @@ export default {
     hardhat: hardhatNetworkSettings,
     localhost: {
       url: "http://127.0.0.1:8545",
-      live: true,
-      //...hardhatNetworkSettings,
+      ...hardhatNetworkSettings,
     },
     tenderly: getCommonNetworkConfig("tenderly", 3030),
     main: getCommonNetworkConfig(eEthereumNetwork.main, 1),
