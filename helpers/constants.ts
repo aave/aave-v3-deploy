fc61ce9719c212819530d9955c312ef73eb2e601
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-03-03 11:20:48 +0000

    add sepolia mock oracle

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -97,6 +97,7 @@ export const chainlinkAggregatorProxy: Record<string, string> = {
   ropsten: "0x12BAaa24D85A4A180F0d5ae67b6aCbDDD58968EA",
   goerli: "0x60E4B131f0F219c72b0346675283E73888e4AB24",
   [eArbitrumNetwork.goerliNitro]: "0xC09e69E79106861dF5d289dA88349f10e2dc6b5C",
+  [eEthereumNetwork.sepolia]: "0x6c60d915c7a646860dba836ffcb7f112b6cfdc76",
 };
 
 export const chainlinkEthUsdAggregatorProxy: Record<string, string> = {
@@ -119,6 +120,7 @@ export const chainlinkEthUsdAggregatorProxy: Record<string, string> = {
   ropsten: "0x12BAaa24D85A4A180F0d5ae67b6aCbDDD58968EA",
   goerli: "0x60E4B131f0F219c72b0346675283E73888e4AB24",
   [eArbitrumNetwork.goerliNitro]: "0xC09e69E79106861dF5d289dA88349f10e2dc6b5C",
+  [eEthereumNetwork.sepolia]: "0x6c60d915c7a646860dba836ffcb7f112b6cfdc76",
 };
 
 export const ETHEREUM_SHORT_EXECUTOR =
